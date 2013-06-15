b4a3588ec186670606fde08ed9aa49e468b5de32
airjulio <juliocezar.rodrigues@gmail.com>
2013-06-15 16:05:37 +0200

    Img comparison using GPU similar to serial comparator. Only partially parallel.

--- MatchGraph/src/Comparator_CVGPU.h
+++ MatchGraph/src/Comparator_CVGPU.h
@@ -11,6 +11,17 @@
 class ComparatorCVGPU {
 public:
 	int compareGPU(char* img1, char* img2, bool showMatches=true, bool drawEpipolar=false);
+    
+    int ratioTest(std::vector< std::vector<cv::DMatch> >& matches);
+    
+	void symmetryTest(const std::vector< std::vector<cv::DMatch> >& matches1,
+                      const std::vector< std::vector<cv::DMatch> >& matches2,
+                      std::vector<cv::DMatch>& symMatches);
+    
+    cv::Mat ransacTest(const std::vector<cv::DMatch>& matches,
+                       const std::vector<cv::KeyPoint>& keypoints1,
+                       const std::vector<cv::KeyPoint>& keypoints2,
+                       std::vector<cv::DMatch>& outMatches);
 
 	~ComparatorCVGPU(){};
 };
