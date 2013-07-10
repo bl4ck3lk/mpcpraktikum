cfe94fee15102a2febf5375b2507dc2caee99c48
Fabian Schwarzkopf <schwarzk@cgpool1207.Informatik.Uni-Tuebingen.De>
2013-07-10 15:58:24 +0200

    Refactored Comparator.

--- MatchGraph/src/Comparator_CVGPU.h
+++ MatchGraph/src/Comparator_CVGPU.h
@@ -8,6 +8,7 @@
 #ifndef COMPARATORCVGPU_H_
 #define COMPARATORCVGPU_H_
 
+#include "ImageComparator.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp> //This is where actual SURF and SIFT algorithm is located
@@ -15,23 +16,29 @@
 #include <opencv2/calib3d/calib3d.hpp> // for homography
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/gpu/gpu.hpp>
+#include <opencv2/nonfree/gpu.hpp>
 #include <vector>
 
 class ComparatorCVGPU {
 public:
-	int compareGPU(const char* img1, const char* img2, bool showMatches=true, bool drawEpipolar=false);
-    
-    int ratioTest(std::vector< std::vector<cv::DMatch> >& matches);
+	//int compareGPU(char* img1, char* img2, bool showMatches=true, bool drawEpipolar=false);
+    	int compareGPU(ImageHandler* iHandler, int* h_idx1,int* h_idx2, int* h_result, int k, bool showMatches, bool drawEpipolar);
+    	int ratioTest(std::vector< std::vector<cv::DMatch> >& matches);
     
 	void symmetryTest(const std::vector< std::vector<cv::DMatch> >& matches1,
                       const std::vector< std::vector<cv::DMatch> >& matches2,
                       std::vector<cv::DMatch>& symMatches);
     
-    cv::Mat ransacTest(const std::vector<cv::DMatch>& matches,
+    	cv::Mat ransacTest(const std::vector<cv::DMatch>& matches,
                        const std::vector<cv::KeyPoint>& keypoints1,
                        const std::vector<cv::KeyPoint>& keypoints2,
                        std::vector<cv::DMatch>& outMatches);
 
+	void match2(cv::gpu::GpuMat& im1_descriptors_gpu, 
+				cv::gpu::GpuMat& im2_descriptors_gpu,
+				std::vector<cv::DMatch>& symMatches);
+
+	struct IMG uploadImage(const int img, cv::gpu::SURF_GPU& surf, ImageHandler* iHandler);
 	~ComparatorCVGPU(){};
 };
 
