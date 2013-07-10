a6270fc174b24c1361495db2d88f9ad7d0615a0b
Fabian Schwarzkopf <schwarzk@cgpool1207.Informatik.Uni-Tuebingen.De>
2013-07-10 11:40:09 +0200

    Delivers now random indices of the whole dimension of the matrix.

--- MatchGraph/src/Initializer.cu
+++ MatchGraph/src/Initializer.cu
@@ -34,25 +34,25 @@
 const int THREADS = 128;
 
 //Initialize index arrays
-static __global__ void initIndexArrays(int* d_idx1, int* d_idx2, int* d_res, int size)
+static __global__ void initIndexArrays(int* d_idx1, int* d_idx2, int* d_res, int dim, int initArraySize)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (idx < size)
+	if (idx < dim)
 	{
 		d_idx1[idx] = idx;
 		d_idx2[idx] = idx;
-		d_res[idx] = 0;
+		if (idx < initArraySize) d_res[idx] = 0;
 	}
 }
 
 //Simple kernel to shuffle an array on GPU (prevents memcpy). To be called only with ONE thread!
-static __global__ void shuffleArrayKernel(int* array, int dim, curandState* globalStates, unsigned long seed)
+static __global__ void shuffleArrayKernel(int* array, int dim, int initArraySize, curandState* globalStates, unsigned long seed)
 {
 	int idx = threadIdx.x;
 	curand_init(seed, idx, 0, &globalStates[idx]);
 	curandState localState = globalStates[idx];
-	for(int i = 0; i < dim; i++)
+	for(int i = 0; i < initArraySize; i++) //just for the needed index array size
 	{
 		int shift = curand_uniform(&localState) * dim;
 		int shift_idx = (i+shift)%dim;
@@ -64,11 +64,11 @@ static __global__ void shuffleArrayKernel(int* array, int dim, curandState* glob
 }
 
 //Kernel to swap upper diagonal matrix elements
-static __global__ void swapUpperDiagonal(int* idx1, int* idx2, int dim)
+static __global__ void swapUpperDiagonal(int* idx1, int* idx2, int initArraySize)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (idx < dim)
+	if (idx < initArraySize)
 	{
 		//swap upper diagonal elements
 		if (idx1[idx] <= idx2[idx])
@@ -81,11 +81,11 @@ static __global__ void swapUpperDiagonal(int* idx1, int* idx2, int dim)
 }
 
 //check for symmetric entries (duplicates caused by swapping earlier)
-static __global__ void checkForDuplicates(int* idx1, int* idx2, int size, int dim)
+static __global__ void checkForDuplicates(int* idx1, int* idx2, int initArraySize, int dim)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (idx < (size-1))
+	if (idx < (initArraySize-1))
 	{
 		if (idx1[idx] == idx2[idx]) //diagonal element (can not occur twice)
 		{
@@ -96,7 +96,7 @@ static __global__ void checkForDuplicates(int* idx1, int* idx2, int size, int di
 		else
 		{
 			int compareIdx = idx2[idx];
-			for(int j = idx+1; j < size && idx1[idx] == idx1[j]; j++)
+			for(int j = idx+1; j < initArraySize && idx1[idx] == idx1[j]; j++)
 			{
 				if (compareIdx == idx2[j])
 				{
@@ -126,67 +126,79 @@ Initializer::~Initializer()
  */
 void Initializer::doInitializationPhase(MatrixHandler* T, ImageHandler* iHandler, ImageComparator* comparator, int initArraySize)
 {
+	bool debugPrint = false;
+
+	unsigned int dim = T->getDimension();
+
+	if (initArraySize > dim)
+	{
+		printf("[INITIALIZER]: initialization array size must be smaller than dimension!\n");
+		exit(EXIT_FAILURE);
+	}
+
 	//Initialization arrays
 	int* d_initIdx1;
 	int* d_initIdx2;
 	int* d_initRes;
-	cudaMalloc(&d_initIdx1, initArraySize*sizeof(int));
-	cudaMalloc(&d_initIdx2, initArraySize*sizeof(int));
+	cudaMalloc(&d_initIdx1, dim*sizeof(int));
+	cudaMalloc(&d_initIdx2, dim*sizeof(int));
 	cudaMalloc(&d_initRes, initArraySize*sizeof(int));
 
-	//initialize arrays
-	int numBlocks = (initArraySize + THREADS - 1) / THREADS;
-	dim3 threadBlock(THREADS);
-	dim3 blockGrid(numBlocks);
-	initIndexArrays<<<blockGrid, threadBlock>>>(d_initIdx1, d_initIdx2, d_initRes, initArraySize);
+	//initialize these arrays
+	int numBlocks = (dim + THREADS - 1) / THREADS;
+	initIndexArrays<<<numBlocks, THREADS>>>(d_initIdx1, d_initIdx2, d_initRes, dim, initArraySize);
 	CUDA_CHECK_ERROR()
 
-	//todo remove debug printing
-	int* testResult1 = new int[initArraySize];
-	int* testResult2 = new int[initArraySize];
+	int* testResult1 = new int[dim];
+	int* testResult2 = new int[dim];
 	int* testResult3 = new int[initArraySize];
-	/*
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult3, d_initRes, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	Tester::printArrayInt(testResult3, initArraySize);
-	*/
+	//todo remove debug printing
+	if (debugPrint)
+	{
+		printf("Init-Index-Array initialization\n");
+		cudaMemcpy(testResult1, d_initIdx1, dim*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, dim*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult3, d_initRes, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, dim);
+		Tester::printArrayInt(testResult2, dim);
+		Tester::printArrayInt(testResult3, initArraySize);
+	}
 
 	//initialize cuRAND
 	curandState* states;
 	cudaMalloc(&states, sizeof(curandState));
 
 	//shuffle Arrays
-	//todo consider streaming?
-	shuffleArrayKernel<<<1, 1>>>(d_initIdx1, initArraySize, states, time(NULL));
+	shuffleArrayKernel<<<1, 1>>>(d_initIdx1, dim, initArraySize, states, time(NULL));
 	CUDA_CHECK_ERROR()
-	shuffleArrayKernel<<<1, 1>>>(d_initIdx2, initArraySize, states, 3*time(NULL)); //ensure different seeds
+	shuffleArrayKernel<<<1, 1>>>(d_initIdx2, dim, initArraySize, states, 3*time(NULL)); //ensure different seeds
 	CUDA_CHECK_ERROR()
 
 	//todo remove debug printing
-	/*
-	printf("shuffled\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	*/
+	if (debugPrint)
+	{
+		printf("shuffled\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+	}
 
 	//mask each entry on the upper diagonal matrix (prevents symmetrical image comparisons)
 	//swap entries inside upper diagonal-matrix (map to lower diagonal matrix)
-	swapUpperDiagonal<<<blockGrid, threadBlock>>>(d_initIdx1, d_initIdx2, initArraySize);
+	numBlocks = (initArraySize + THREADS - 1) / THREADS;
+	swapUpperDiagonal<<<numBlocks, THREADS>>>(d_initIdx1, d_initIdx2, initArraySize);
 	CUDA_CHECK_ERROR()
 
 	//todo remove debug printing
-	/*
-	printf("swapped\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	*/
+	if (debugPrint)
+	{
+		printf("swapped\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+	}
 
 	//sort index array1 ascending and index array2 respectively
 	//wrap device pointers
@@ -197,39 +209,42 @@ void Initializer::doInitializationPhase(MatrixHandler* T, ImageHandler* iHandler
 	CUDA_CHECK_ERROR();
 
 	//todo remove debug printing
-	/*
-	printf("sorted\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	*/
+	if (debugPrint)
+	{
+		printf("sorted\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+	}
 
-	//check for duplicated enries and maks them out
-	checkForDuplicates<<<blockGrid, threadBlock>>>(d_initIdx1, d_initIdx2, initArraySize, T->getDimension());
+	//check for duplicated entries and mask them out
+	checkForDuplicates<<<numBlocks, THREADS>>>(d_initIdx1, d_initIdx2, initArraySize, dim);
 	CUDA_CHECK_ERROR();
 
 	//todo remove debug printing
-	/*
-	printf("masked duplicates\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	*/
+	if (debugPrint)
+	{
+		printf("masked duplicates\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+	}
 
 	//sort again
 	thrust::sort_by_key(dp_initIdx1, dp_initIdx1 + initArraySize, dp_initIdx2); //ascending
 	CUDA_CHECK_ERROR();
 
 	//todo remove debug printing
-	/*
-	printf("sorted\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	*/
+	if (debugPrint)
+	{
+		printf("sorted\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+	}
 
 	//compare images
 	cudaMemcpy(testResult3, d_initRes, initArraySize*sizeof(int), cudaMemcpyDeviceToHost); //todo remove me (just for testing)
@@ -238,25 +253,31 @@ void Initializer::doInitializationPhase(MatrixHandler* T, ImageHandler* iHandler
 	cudaMemcpy(d_initRes, testResult3, initArraySize*sizeof(int), cudaMemcpyHostToDevice); //todo remove me (just for testing)
 
 	//todo remove debug printing
-	printf("result after comparison -> this will be given to GPUSparse to update the Marix\n");
-	cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(testResult3, d_initRes, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
-	Tester::printArrayInt(testResult1, initArraySize);
-	Tester::printArrayInt(testResult2, initArraySize);
-	Tester::printArrayInt(testResult3, initArraySize);
+	if (debugPrint)
+	{
+		printf("result after comparison -> this will be given to GPUSparse to update the Marix\n");
+		cudaMemcpy(testResult1, d_initIdx1, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult2, d_initIdx2, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(testResult3, d_initRes, initArraySize*sizeof(int), cudaMemcpyDeviceToHost);
+		Tester::printArrayInt(testResult1, initArraySize);
+		Tester::printArrayInt(testResult2, initArraySize);
+		Tester::printArrayInt(testResult3, initArraySize);
+	}
+
 
 	//initialize T Matrix with compared images
 	//invoked only on sparse matrixhandler
 	GPUSparse* T_sparse = dynamic_cast<GPUSparse*> (T);
 	T_sparse->updateSparseStatus(d_initIdx1, d_initIdx2, d_initRes, initArraySize);
 
-
-
 	//cleanup after completed initialization
 	cudaFree(d_initIdx1);
 	cudaFree(d_initIdx2);
 	cudaFree(d_initRes);
+
+	delete[] testResult1;
+	delete[] testResult2;
+	delete[] testResult3;
 }
 
 
